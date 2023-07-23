2700177577ca7baf24046d76644a48102f4b1d9d
unknown <ekobudi029@gmail.com>
2023-07-23 21:09:53 +0700

    register sidebar vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import "@protonemedia/laravel-splade/dist/style.css";
 
 import { createApp } from "vue/dist/vue.esm-bundler.js";
 import { renderSpladeApp, SpladePlugin } from "@protonemedia/laravel-splade";
+import Sidebar from "./components/Sidebar.vue";
 
 const el = document.getElementById("app");
 
@@ -15,4 +16,5 @@ createApp({
         "transform_anchors": false,
         "progress_bar": true
     })
+    .component("sidebar", Sidebar)
     .mount(el);
