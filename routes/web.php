e3910f83a1a4821e60c8dbbfc5c917bbc009ba90
unknown <ekobudi029@gmail.com>
2023-07-23 21:10:43 +0700

    add route admin controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -41,5 +42,9 @@ Route::middleware('splade')->group(function () {
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
 
-    require __DIR__.'/auth.php';
+    Route::middleware('auth')->group(function () {
+        Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    });
+
+    require __DIR__ . '/auth.php';
 });
