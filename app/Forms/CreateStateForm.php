283c57667501f3cdbc3aaec67094f2ad00ef0116
unknown <ekobudi029@gmail.com>
2023-08-20 21:38:31 +0700

    Create Form builder splade for state created form

---
+++ app/Forms/CreateStateForm.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Forms;
+
+use App\Models\Country;
+use ProtoneMedia\Splade\SpladeForm;
+use ProtoneMedia\Splade\AbstractForm;
+use ProtoneMedia\Splade\FormBuilder\Text;
+use ProtoneMedia\Splade\FormBuilder\Select;
+use ProtoneMedia\Splade\FormBuilder\Submit;
+
+class CreateStateForm extends AbstractForm
+{
+    public function configure(SpladeForm $form)
+    {
+        $form
+            ->action(route('admin.states.store'))
+            ->method('POST')
+            ->class('space-y-4 bg-white rounded p-4')
+            ->fill([
+                //
+            ]);
+    }
+
+    public function fields(): array
+    {
+        return [
+            Text::make('name')
+                ->label(__('Name'))
+                ->rules(['required', 'max:100', 'min:3']),
+            Select::make('country_id')
+                ->label('Choose a country')
+                ->options(Country::pluck('name', 'id')->toArray())
+                ->rules(['required']),
+
+            //
+
+            Submit::make()
+                ->label(__('Save')),
+        ];
+    }
+}
