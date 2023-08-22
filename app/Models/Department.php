42d658c9f3aed89538b9c0abf31b9254617b0440
unknown <ekobudi029@gmail.com>
2023-08-22 21:33:36 +0700

    add  protected $fillable

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Departmet extends Model
+class Department extends Model
 {
     use HasFactory;
+    protected $fillable = ['name'];
 }
