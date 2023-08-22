02d7183e997dd1dce747c4af029620af748a8436
unknown <ekobudi029@gmail.com>
2023-08-22 21:33:57 +0700

    create tables splade for cities

---
+++ app/Tables/Cities.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Tables;
+
+use App\Models\City;
+use App\Models\State;
+use Illuminate\Http\Request;
+use ProtoneMedia\Splade\SpladeTable;
+use Spatie\QueryBuilder\QueryBuilder;
+use ProtoneMedia\Splade\AbstractTable;
+use Spatie\QueryBuilder\AllowedFilter;
+use Illuminate\Database\Eloquent\Collection;
+
+class Cities extends AbstractTable
+{
+    /**
+     * Create a new instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the user is authorized to perform bulk actions and exports.
+     *
+     * @return bool
+     */
+    public function authorize(Request $request)
+    {
+        return true;
+    }
+
+    /**
+     * The resource or query builder.
+     *
+     * @return mixed
+     */
+    public function for()
+    {
+        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
+            $query->where(function ($query) use ($value) {
+                Collection::wrap($value)->each(function ($value) use ($query) {
+                    $query
+                        ->orWhere('name', 'LIKE', "%{$value}%");
+                });
+            });
+        });
+
+        return QueryBuilder::for(City::class)
+            ->defaultSort('id')
+            ->allowedSorts(['id', 'name'])
+            ->allowedFilters(['id', 'name',  'state_id', $globalSearch]);
+    }
+
+    /**
+     * Configure the given SpladeTable.
+     *
+     * @param \ProtoneMedia\Splade\SpladeTable $table
+     * @return void
+     */
+    public function configure(SpladeTable $table)
+    {
+        $table
+            ->withGlobalSearch(columns: ['name'])
+            ->column('id', sortable: true)
+            ->column('name', sortable: true)
+            ->column(key: 'state.name', label: 'State')
+            // ->rowLink(function (User $user) {
+            //     return route('admin.users.edit', $user);
+            // })
+            ->column('action')
+            ->selectFilter(
+                key: 'state_id',
+                options: State::pluck('name', 'id')->toArray(),
+                label: 'State'
+            )
+            ->paginate(15);
+
+        // ->searchInput()
+        // ->selectFilter()
+        // ->withGlobalSearch()
+
+        // ->bulkAction()
+        // ->export()
+    }
+}
