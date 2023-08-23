6b609696b784eabbc2fca47acb054268b2f1ab14
unknown <ekobudi029@gmail.com>
2023-08-23 21:07:25 +0700

    create employee function index create and store

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
+use App\Models\Employee;
+use App\Tables\Employees;
+use App\Models\Department;
 use Illuminate\Http\Request;
+use ProtoneMedia\Splade\SpladeForm;
+use ProtoneMedia\Splade\Facades\Splade;
+use ProtoneMedia\Splade\FormBuilder\Date;
+use ProtoneMedia\Splade\FormBuilder\Input;
+use ProtoneMedia\Splade\FormBuilder\Select;
+use ProtoneMedia\Splade\FormBuilder\Submit;
+use App\Http\Requests\CreateEmployeeRequest;
 
 class EmployeeController extends Controller
 {
@@ -11,7 +22,9 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.employees.index', [
+            'employees' => Employees::class
+        ]);
     }
 
     /**
@@ -19,15 +32,36 @@ class EmployeeController extends Controller
      */
     public function create()
     {
-        //
+        $form = SpladeForm::make()->action(route('admin.employees.store'))
+            ->fields([
+                Input::make('first_name')->label('First Name'),
+                Input::make('last_name')->label('Last Name'),
+                Input::make('middle_name')->label('Middle Name'),
+                Input::make('zip_code')->label('Zip Code'),
+                Select::make('department_id')->label('Choose a Department')->options(Department::pluck('name', 'id')->toArray()),
+                Select::make('city_id')->label('Choose a City')->options(City::pluck('name', 'id')->toArray()),
+                Date::make('birth_date')->label('Birth Date'),
+                Date::make('hired_date')->label('Hired Date'),
+                Submit::make()->label('Save')
+            ])->class('space-y-4 bg-white rounded p-4');
+        return view('admin.employees.create', [
+            'form' => $form
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateEmployeeRequest $request)
     {
-        //
+        $city = City::findOrFail($request->city_id);
+        Employee::create(array_merge($request->validated(), [
+            'country_id' => $city->state->country_id,
+            'state_id' => $city->state_id,
+        ]));
+
+        Splade::toast('Employee Created')->autoDismiss(3);
+        return to_route('admin.employees.index');
     }
 
     /**
